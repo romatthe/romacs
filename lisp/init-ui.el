22f828ea99fb45a73082d1791ce9f1086a0939ee
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-11-08 01:33:00 +0100

    Included basic UI configuration

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -113,6 +113,9 @@
   (doom-modeline-mu4e nil)
   (doom-modeline-buffer-file-name-style 'relative-from-project))
 
+(use-package hide-mode-line
+  :hook (((completion-list-mode completion-in-region-mode) . hide-mode-line-mode)))
+
 ;; Install the collection of themes from Doom-Emacs
 (use-package doom-themes
   :demand t
@@ -123,6 +126,39 @@
   (load-theme 'doom-gruvbox t)
   (doom-themes-treemacs-config)
   (doom-themes-org-config))
+
+;; Line numbers and columns
+(use-package display-line-numbers
+  :straight nil
+  :blackout t
+  :hook (after-init . global-display-line-numbers-mode))
+
+;; Mouse & Smooth Scroll
+;; Scroll one line at a time (less "jumpy" than defaults))
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
+      mouse-wheel-progressive-speed nil
+      scroll-step 1
+      scroll-margin 0
+      scroll-conservatively 100000)
+
+;; Suppress GUI features
+(setq use-file-dialog nil
+      use-dialog-box nil
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message t)
+
+;; Popup for showing key completions
+(use-package which-key
+  :demand t
+  :config
+  (which-key-mode))
+
+;; Misc
+(fset 'yes-or-no-p 'y-or-n-p)
+(add-hook 'window-setup-hook #'size-indication-mode)
+(setq ring-bell-function 'ignore) ; Don't play a sound or flash the screen on error
+(setq inhibit-compacting-font-caches t) ; Don’t compact font caches during GC.
+
 
 (provide 'init-ui)
 
