3e488b9bbf470508ef883b8b0be19da450a45d75
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-11-08 01:51:40 +0100

    Added support for Projectile

---
+++ lisp/init-projectile.el
@@ -0,0 +1,56 @@
+;;; -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018-2019 Robin Mattheussen
+
+;; Author: Robin Mattheussen <robin.mattheussen@gmail.com>
+;; URL: https://github.com/romatthe/dotfiles
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+;;; Commentary:
+;;
+;; Configuration for the Projectile project management tool
+;;
+
+;;; Code:
+
+
+(use-package projectile
+  :blackout
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map))
+  :hook (after-init . projectile-mode)
+  :custom
+  (projectile-mode-line-prefix "")
+  (projectile-sort-order 'recentf)
+  (projectile-use-git-grep t)
+  :config
+  ;; Use the faster searcher to handle project files: ripgrep `rg'.
+  (when (executable-find "rg")
+    (setq projectile-generic-command
+          (let ((rg-cmd ""))
+            (dolist (dir projectile-globally-ignored-directories)
+              (setq rg-cmd (format "%s --glob '!%s'" rg-cmd dir)))
+            (concat "rg -0 --files --color=never --hidden" rg-cmd)))))
+
+
+(provide 'init-projectile)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-projectile.el ends here
